d2e931b992e50b919b1f2afa565f8ff616d34df9
Sergey Stelmakh <33370467+bubi22@users.noreply.github.com>
2021-04-10 11:15:48 +0300

    item

---
+++ DispWorkItem/DispWorkItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DispWorkItem
+//
+//  Created by днс on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
